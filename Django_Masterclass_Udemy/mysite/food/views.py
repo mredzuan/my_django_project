304d5954998508060b858c8905e6a60fbcf261ea
redz <redzu.m@gmail.com>
2024-06-02 10:39:39 +0800

    cont learn..

--- Django_Masterclass_Udemy/mysite/food/views.py
+++ Django_Masterclass_Udemy/mysite/food/views.py
@@ -1,11 +1,30 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = Item.objects.all()
+
+    context = {
+        "item_list":item_list,
+    }
+    return render(request,"food/index.html", context)
+
 
 
 def item(request):
     return HttpResponse("<h1>This is an item view</h1>")
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        "item":item
+    }
+    return render(request, "food/detail.html", context)
+
+    #return HttpResponse("This is item no/id: %s " % item_id)
